e19d54653efb563c3acc3810652ec2bd549efb01
santoshsol <104581887+santoshsol@users.noreply.github.com>
2022-04-29 18:11:20 +0530

    Add files via upload

---
+++ prog2.java
@@ -0,0 +1,16 @@
+import java .util.*;
+//   Program Titale > Addition of 2 Number Display reslut in main Function 
+//   Date:: 6-01-2022
+class prog2 
+{
+     public static void main (String arg[])
+     {
+         Scanner sobj = new Scanner (System.in);
+
+          int ino1 = 100 , ino2 = 200, ireslut = 0;
+
+          ireslut = ino1 + ino2;
+
+          System.out.println(" Addition is "+ireslut);
+     }
+}
